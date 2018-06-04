99e3eefb6a9feab7b30e101031b8a63cb27d5a59
Gattringer Ivonne <ivonne.gattringer@gmail.com>
2018-06-04 09:15:19 +0200

    saving, complete

--- ivonnespotify.js
+++ ivonnespotify.js
@@ -5,9 +5,16 @@ window.onload = function(){  //Local storage
   store.onclick = function(){
     let artistsSearch = document.getElementById("search-box").value;
     let artists = artistsSearch.split(",");
-    for (var i = 0; i < artists.length; i++) {
-      console.log(artists[i]);
-      localStorage.setItem("Artist"+ i.toString(),artists[i]);
+    let x = 0;
+    var e  = " ";
+    while(e != null){
+      e = localStorage.getItem('Artist' + x.toString());
+
+      x++;
+    }
+    x--;
+    for (var i = x; i < artists.length+x; i++) {
+      localStorage.setItem("Artist"+ i.toString(),artists[i-x]);
     }
   }
   let clearButton = document.getElementById("clear");
